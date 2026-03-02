4c390130dae3333d0ccd44bd34d28e698028a2a9
Andrew Knoll <Aknoll1@mtech.edu>
2026-03-02 12:40:58 -0700

    Added the orientation checker code

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-def add_positive_integers(a: int, b: int) -> int:
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise ValueError("Inputs must be integers")
-    if a < 0 or b < 0:
-        raise ValueError("Inputs must be positive")
-    return a + b
+"""
+Andrew Curtis Knoll
+CSCI 332 Spring 2025
+Programming Assignment #8
+I acknowledge that I have worked on this assignment independently, except where explicitly noted
+and referenced. Any collaboration or use of external resources has been properly cited. I am
+fully aware of the consequences of academic dishonesty and agree to abide by the university's
+academic integrity policy. I understand the importance the consequences of plagiarism.
+"""
 
-i = "test"
+
+
+def orientation(p1:tuple[int, int],  p2: tuple[int, int], p3: tuple[int, int]):
+    val = ((p2[1]-p1[1]) * (p3[0]-p2[0]) - (p2[0]-p1[0]) *(p3[1]-p2[1]))
+
+    if val == 0:
+        return "Linear"
+    
+    if val > 0:
+        return "Clockwise"
+    
+    if val < 0: 
+        return "Counter Clockwise"
+
+
+
+if __name__ == "__main__":
+    p1 = [1, 1]
+    p2 = [2, 2]
+    p3 = [3,3]
+
+    print(orientation(p2,p1,p3))
