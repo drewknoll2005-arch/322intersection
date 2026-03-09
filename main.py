80a88b06a40729c77d2fc9b2668a180c2b104a2e
Andrew Knoll <Aknoll1@mtech.edu>
2026-03-09 12:36:47 -0600

    Finished do_intersection function, and added tests

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ academic integrity policy. I understand the importance the consequences of plagi
 
 
 def orientation(p1:tuple[int, int],  p2: tuple[int, int], p3: tuple[int, int]):
-    val = ((p2(1)-p1(1)) * (p3(0)-p2(0)) - (p2(0)-p1(0)) *(p3(1)-p2(1)))
+    val = ((p2[1]-p1[1]) * (p3[0]-p2[0]) - (p2[0]-p1[0]) *(p3[1]-p2[1]))
 
     if val == 0:
         return 0 #Colinear
@@ -23,21 +23,45 @@ def orientation(p1:tuple[int, int],  p2: tuple[int, int], p3: tuple[int, int]):
         return 2 #Counter Clockwise
     
 def on_segment(p1:tuple[int, int],  p2: tuple[int, int], p3: tuple[int, int]): #Returns True if point p2 lies on segment pr.
-    cross_product = (p3(0) - p1(0)) * (p2(1) - p1(1)) - (p3(1) - p1(1)) * (p2(0) - p1(0))
+    cross_product = (p3[0] - p1[0]) * (p2[1] - p1[1]) - (p3[1] - p1[1]) * (p2[0] - p1[0])
    
-    if p1(0) < p2(0) and p1(1) < p2(1) and p2(0) < p3(0) and p2(1) < p3(1) and cross_product == 0:
+    if p1[0] < p2[0] and p1[1] < p2[1] and p2[0] < p3[0] and p2[1] < p3[1] and cross_product == 0:
         return True
     else:
         return False
     
 def do_intersection(seg1: tuple[tuple[int, int], tuple[int, int]], seg2: tuple[tuple[int, int], tuple[int, int]]):
+    x1 = seg1[0][0]
+    y1 = seg1[0][1]
+    x2 = seg1[1][0]
+    y2 = seg1[1][1]
+    x3 = seg2[0][0]
+    y3 = seg2[0][1]
+    x4 = seg2[1][0]
+    y4 = seg2[1][1]
+
+    denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1)
+
+    if denom == 0:
+        return None 
+
+    ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom
     
+    x = x1 + ua * (x2 - x1)
+    y = y1 + ua * (y2 - y1)
+    return (x, y)
 
 
 if __name__ == "__main__":
     p1 = (1, 1)
     p2= (2, 2)
     p3 = (3,3)
-
+    p4 = (-10,-10)
+    p5 = (10,10)
+    p6 = (-10,10)
+    p7 = (10,-10)
+    seg1 = (p4,p5)
+    seg2 = (p6,p7)
     print(orientation(p1,p2,p3))
     print(on_segment(p1,p2,p3))
+    print(do_intersection(seg1,seg2))
--- tests.py
+++ tests.py
@@ -1,17 +1,53 @@
 import unittest
-from main import add_positive_integers
+from main import orientation, on_segment, do_intersection
 
 class TestMathFunctions(unittest.TestCase):
-    def test_sum_correct(self):
-        self.assertEqual(add_positive_integers(5, 10), 15)
+    def test_orientation_Colinear(self):
+        p1 = (1, 1)
+        p2= (2, 2)
+        p3 = (3,3)
+        self.assertEqual(orientation(p1,p2,p3), 0)
 
-    def test_negative_input(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers(-1, 5)
+    def test_orientation_Clockwise(self):
+        p1 = (1, 1)
+        p2= (2, 2)
+        p3 = (3,2)
+        self.assertEqual(orientation(p1,p2,p3), 1)
 
-    def test_wrong_type(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers("5", 5)
+    def test_orientation_Counter_Clockwise(self):
+        p1 = (1, 1)
+        p2= (2, 2)
+        p3 = (3,4)
+        self.assertEqual(orientation(p1,p2,p3), 2)
+        
+    def test_on_segment_true(self):
+        p1 = (1, 1)
+        p2= (2, 2)
+        p3 = (3,3)
+        self.assertEqual(on_segment(p1,p2,p3), True)
 
+    def test_on_segment_false(self):
+        p1 = (1, 1)
+        p2= (2, 3)
+        p3 = (3,3)
+        self.assertEqual(on_segment(p1,p2,p3), False)
+
+    def test_do_intersection(self):
+        p4 = (-10,-10)
+        p5 = (10,10)
+        p6 = (-10,10)
+        p7 = (10,-10)
+        seg1 = (p4,p5)
+        seg2 = (p6,p7)
+        self.assertEqual(do_intersection(seg1,seg2), ((0.0, 0.0)))
+    
+    def test_do_intersection_parrelel(self):
+        p4 = (-10,-10)
+        p5 = (10,10)
+        p6 = (-11,-11)
+        p7 = (11, 11)
+        seg1 = (p4,p5)
+        seg2 = (p6,p7)
+        self.assertEqual(do_intersection(seg1,seg2), None)
 if __name__ == "__main__":
     unittest.main()
